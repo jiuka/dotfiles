b96b9749b7d9f104355bef643b7f69e5d7a54693
Marius Rieder <marius.rieder@durchmesser.ch>
2012-09-29 19:00:04 +0200

    Add svn config

--- install.sh
+++ install.sh
@@ -4,25 +4,18 @@ function df_install {
     SOURCE=`readlink -f $1`
     TARGET=`readlink -f $2`
 
-    echo "SRC:$SOURCE"
-    echo "TRG:$TARGET"
-
     echo -n "Install '$2': ";
     if [ ! -e $TARGET ]; then
-        echo "::1"
         ln -s $SOURCE $TARGET;
         echo "OK";
         return;
     fi
     if [ -L $2 ]; then
-        echo "::2"
         if [ $SOURCE == $TARGET ]; then
-            echo "::3"
             echo "Skip"
             return;
         fi
     fi
-    echo "::4"
     mkdir -p ~/.df_backup
     mv $TARGET ~/.df_backup/
     ln -s $SOURCE $TARGET;
@@ -34,3 +27,4 @@ df_install gitconfig ~/.gitconfig
 df_install vim ~/.vim
 df_install vimrc ~/.vimrc
 df_install themes ~/.themes
+df_install svnconfig ~/.subversion/config
